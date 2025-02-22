d37cf46c20e9566c15124100b6bcfd40f48f864d
sumia <sumia@naver.com>
2025-02-22 19:40:34 +0900

    개선

--- scenes/start.gd
+++ scenes/start.gd
@@ -5,24 +5,22 @@ extends Node2D
 func _ready() -> void:
 	BgMusic.play_music_intro()
 	$AnimationPlayer.play("Start/FadeIn")
-	await get_tree().create_timer(1.0).timeout
+	await $AnimationPlayer.animation_finished
 	$StartFadeIn.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_game_start_pressed() -> void:
 	BgMusic.stop_music_intro()
-	$AnimationPlayer.play("Start/FadeOut")
 	$AnimationPlayer.play("Start/ButtonEffect")
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer.play("Start/FadeOut")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://scenes/tile_map.tscn")
 
-
 func _on_options_pressed() -> void:
-	$AnimationPlayer.play("Start/FadeOut")
 	$AnimationPlayer.play("Start/ButtonEffect")
 	await $AnimationPlayer.animation_finished
 	pass # Replace with function body.
